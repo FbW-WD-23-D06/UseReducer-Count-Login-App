fa19af2d2ce095d4da8f9791e381e34bd801319a
rami <rami_zezo1986@hotmail.com>
2024-01-11 10:33:59 +0100

    usereducer live coding counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useReducer, useState } from 'react'
 import './App.css'
 
+const reducer = (counter , action)=>{
+  console.log("action",action);
+  // console.log("state",state);
+  switch(action.type){
+    case "increment":{
+      return { count : counter.count + 1};
+    }
+    case "decrement":{
+      return {count : counter.count - 1}
+    }
+  }
+}
 function App() {
-  const [count, setCount] = useState(0)
+  // initialState object
+  const initialState = {count : 0}
+  // const [counter, setCounter] = useState(0);
+  const [counter, dispatch]= useReducer(reducer, initialState);
+  
+  console.log(counter);
+  // const increament = ()=>{
+  //   setCounter(counter + 1)
+  // }
+  // const decreament = ()=>{
+  //   setCounter(counter - 1)
+  // }
+
+  const increament = ()=>{
+    dispatch({type:"increment"})
+  }
+  const decreament = ()=>{
+    dispatch({type:"decrement"})
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <button onClick={increament}>+</button>
+      <h1>{counter.count}</h1>
+      <button onClick={decreament}>-</button>
+   </div>
     </>
   )
 }
